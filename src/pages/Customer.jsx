998e81741ddac142d844af70cddbe39840dc9372
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-11 13:50:04 +0530

    Customer Management Refactored

--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Container, Form, Button} from 'react-bootstrap';
 import axios from 'axios';
 
 function Customer() {
@@ -22,23 +23,39 @@ function Customer() {
                 console.log("Customer Updated Successfully...!");
             })
             .catch((error) => {
-                console.log("Customer Update Error...!");
+                console.log("Customer Updated Error...!");
             });
     };
 
     return (
-        <div>
-            <h1>Customer Management</h1>
-            <form>
-                <input id="id" placeholder="Customer ID" value={customerData.id} onChange={handleChange}/><br/>
-                <input id="name" placeholder="Customer Name" value={customerData.name} onChange={handleChange}/><br/>
-                <input id="address" placeholder="Customer Address" value={customerData.address}
-                       onChange={handleChange}/><br/>
-                <input id="salary" placeholder="Customer Salary" value={customerData.salary}
-                       onChange={handleChange}/><br/>
-                <button id="btnUpdateCustomer" type="button" onClick={updateCustomer}>Update</button>
-            </form>
-        </div>
+        <Container>
+            <h1 className="m-4">Customer Management</h1>
+            <Form>
+                <Form.Group controlId="id">
+                    <Form.Control className="mb-2" type="text" placeholder="Customer ID" value={customerData.id}
+                                  onChange={handleChange}/>
+                </Form.Group>
+                <Form.Group controlId="name">
+                    <Form.Control className="mb-2" type="text" placeholder="Customer Name" value={customerData.name}
+                                  onChange={handleChange}/>
+                </Form.Group>
+                <Form.Group controlId="address">
+                    <Form.Control className="mb-2" type="text" placeholder="Customer Address"
+                                  value={customerData.address}
+                                  onChange={handleChange}/>
+                </Form.Group>
+                <Form.Group controlId="salary">
+                    <Form.Control className="mb-2" type="text" placeholder="Customer Salary" value={customerData.salary}
+                                  onChange={handleChange}/>
+                </Form.Group>
+
+                <Button id="btnSaveCustomer" className={"m-1"} variant="primary" type="button">Save</Button>
+                <Button id="btnUpdateCustomer" className={"m-1"} variant="warning" type="button"
+                        onClick={updateCustomer}>Update</Button>
+                <Button id="btndeleteCustomer" className={"m-1"} variant="danger" type="button">Delete</Button>
+                <Button id="btnGetAllCustomers" className={"m-1"} variant="success" type="button">Get All</Button>
+            </Form>
+        </Container>
     );
 }
 
