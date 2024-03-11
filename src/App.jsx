b9a3933527ca92ee64f2f58c7c7d7947d6145245
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-11 10:23:39 +0530

    App.jsx Added

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Customer from "./pages/Customer";
+import Item from "./pages/Item";
+import Orders from "./pages/Orders";
+import OrderView from "./pages/OrderView";
+import NavBar from "./components/Navigation/NavBar";
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar/>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/customer" element={<Customer/>}/>
+                    <Route path="/item" element={<Item/>}/>
+                    <Route path="/orders" element={<Orders/>}/>
+                    <Route path="/orderview" element={<OrderView/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
