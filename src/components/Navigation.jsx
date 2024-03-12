986f54ffdf74fc406a3afe44badd90747613d18f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-12 12:31:13 +0530

    NavBar Refactored

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,10 +4,12 @@ function Navigation() {
     return (
         <Navbar expand="lg" className="bg-dark navbar-dark">
             <Container>
-                <Navbar.Brand href="#home">React | POS</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <span className='navBrand'>React</span> | POS
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
                         <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/customer">Customers</Nav.Link>
                         <Nav.Link href="/item">Items</Nav.Link>
