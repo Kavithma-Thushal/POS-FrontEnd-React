c6f49ae0046a411fef8d247499f9d1c08ad61e08
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-24 18:59:36 +0530

    App.jsx Refactored

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,17 @@ import OrdersView from "./views/OrdersView";
 import OrderDetailsView from "./views/OrderDetailsView";
 import Navigation from "./components/Navigation";
 
-function App() {
+export default function App() {
     return (
-        <div>
-            <BrowserRouter>
-                <Navigation />
-                <Routes>
-                    <Route path="/home" element={<HomeView />} />
-                    <Route path="/customer" element={<CustomerView />} />
-                    <Route path="/item" element={<ItemView />} />
-                    <Route path="/orders" element={<OrdersView />} />
-                    <Route path="/orderview" element={<OrderDetailsView />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Navigation />
+            <Routes>
+                <Route path="/home" element={<HomeView />} />
+                <Route path="/customer" element={<CustomerView />} />
+                <Route path="/item" element={<ItemView />} />
+                <Route path="/orders" element={<OrdersView />} />
+                <Route path="/orderview" element={<OrderDetailsView />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
-
-export default App;
