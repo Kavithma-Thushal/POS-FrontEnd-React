84d445239b6998183adec600db3ada31246fc146
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-30 12:18:30 +0530

    Customer Validations Added

--- src/controllers/CustomerController.jsx
+++ src/controllers/CustomerController.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { successAlert, errorAlert, emptyMessage } from '../util/Alert';
 
 export function CustomerController() {
@@ -9,10 +9,6 @@ export function CustomerController() {
     useEffect(() => {
         loadAllCustomers();
     }, []);
-
-    const handleChange = (e) => {
-        setCustomerData({ ...customerData, [e.target.id]: e.target.value });
-    };
 
     const [customerData, setCustomerData] = useState({
         id: '',
@@ -24,6 +20,44 @@ export function CustomerController() {
     const [searchCusById, setSearchCusById] = useState('');
     const [allCustomers, setAllCustomers] = useState([]);
     const [disableEnable, setDisableEnable] = useState(false);
+    const [errors, setErrors] = useState('');
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        const regExCusId = /^(C00-)[0-9]{3}$/;
+        const regExCusName = /^[A-z ]{3,20}$/;
+        const regExCusAddress = /^[A-Za-z0-9/, -]{4,30}$/;
+        const regExCusSalary = /^[0-9]{1,}[.]?[0-9]{2}$/;
+
+        let errorMessage = '';
+        switch (id) {
+            case 'id':
+                if (!regExCusId.test(value)) {
+                    errorMessage = 'Customer id must match the pattern C00-001';
+                }
+                break;
+            case 'name':
+                if (!regExCusName.test(value)) {
+                    errorMessage = 'Customer name must be between 3-20 characters';
+                }
+                break;
+            case 'address':
+                if (!regExCusAddress.test(value)) {
+                    errorMessage = 'Customer address must be between 4-30 characters';
+                }
+                break;
+            case 'salary':
+                if (!regExCusSalary.test(value)) {
+                    errorMessage = 'Customer salary must have 3 digits with 2 decimal places';
+                }
+                break;
+            default:
+                break;
+        }
+
+        setCustomerData({ ...customerData, [id]: value });
+        setErrors({ ...errors, [id]: errorMessage });
+    };
 
     const saveCustomer = () => {
         axios.post(baseUrl + 'customer', customerData)
@@ -137,6 +171,7 @@ export function CustomerController() {
         loadAllCustomers,
         tableListener,
         disableEnable,
-        clearInputFields
+        clearInputFields,
+        errors
     };
 }
