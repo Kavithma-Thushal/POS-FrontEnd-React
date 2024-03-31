21573951604e5a1587f168418edbbc2a10e8f8eb
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-31 23:34:04 +0530

    Manage Items Successfully

---
+++ src/controllers/ItemController.jsx
@@ -0,0 +1,187 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { successAlert, errorAlert, emptyMessage } from '../util/Alert';
+
+export function ItemController() {
+
+    let baseUrl = "http://localhost:8080/spring_pos/";
+
+    useEffect(() => {
+        loadAllItems();
+    }, []);
+
+    const regExItemCode = /^(I00-)[0-9]{3}$/;
+    const regExItemDescription = /^[A-z ]{3,20}$/;
+    const regExItemQty = /^[0-9]{2,10}$/;
+    const regExItemUnitPrice = /^[0-9]{1,}[.]?[0-9]{1}$/;
+
+    const [itemData, setItemData] = useState({
+        code: '',
+        description: '',
+        qty: '',
+        unitPrice: ''
+    });
+    const [itemValidations, setItemValidations] = useState('');
+    const [searchItemByCode, setSearchItemByCode] = useState('');
+    const [allItems, setAllItems] = useState([]);
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        let errorMessage = '';
+
+        switch (id) {
+            case 'code':
+                if (!regExItemCode.test(value)) {
+                    errorMessage = 'Item code must match the pattern I00-001';
+                }
+                break;
+            case 'description':
+                if (!regExItemDescription.test(value)) {
+                    errorMessage = 'Item description must be between 3-20 characters';
+                }
+                break;
+            case 'qty':
+                if (!regExItemQty.test(value)) {
+                    errorMessage = 'Item quantity must have 2-10 digits';
+                }
+                break;
+            case 'unitPrice':
+                if (!regExItemUnitPrice.test(value)) {
+                    errorMessage = 'Item price must have 2 digits with 2 decimal places';
+                }
+                break;
+            default:
+                break;
+        }
+
+        setItemValidations({ ...itemValidations, [id]: errorMessage });
+        setItemData({ ...itemData, [id]: value });
+    };
+
+    const saveItem = () => {
+        const isValidCode = regExItemCode.test(itemData.code);
+        const isValidDescription = regExItemDescription.test(itemData.description);
+        const isValidQty = regExItemQty.test(itemData.qty);
+        const isValidUnitPrice = regExItemUnitPrice.test(itemData.unitPrice);
+
+        if (isValidCode && isValidDescription && isValidQty && isValidUnitPrice) {
+            axios.post(baseUrl + 'item', itemData)
+                .then((resp) => {
+                    successAlert("Item", resp.data.message);
+                    loadAllItems();
+                })
+                .catch((error) => {
+                    errorAlert("Item", error.message);
+                });
+        } else {
+            errorAlert("All Details Required - ");
+        }
+    };
+
+    const searchItem = () => {
+        axios.get(baseUrl + 'item/searchItem/' + searchItemByCode)
+            .then((resp) => {
+                setAllItems([]);
+                setAllItems([{
+                    code: resp.data.code,
+                    description: resp.data.description,
+                    qty: resp.data.qty,
+                    unitPrice: resp.data.unitPrice
+                }]);
+                clearInputFields();
+            })
+            .catch((error) => {
+                emptyMessage(error.message);
+                clearInputFields();
+                loadAllItems();
+            });
+    };
+
+    const updateItem = () => {
+        axios.put(baseUrl + 'item', itemData)
+            .then((resp) => {
+                successAlert("Item", resp.data.message);
+                loadAllItems();
+            })
+            .catch((error) => {
+                errorAlert("Item", error.message);
+            });
+    };
+
+    const deleteItem = () => {
+        axios.delete(baseUrl + 'item/' + itemData.code)
+            .then((resp) => {
+                successAlert("Item", resp.data.message);
+                loadAllItems();
+            })
+            .catch((error) => {
+                errorAlert("Item", error.message);
+            });
+    };
+
+    const loadAllItems = () => {
+        axios.get(baseUrl + 'item/loadAllItems')
+            .then((resp) => {
+                setAllItems(resp.data.data);
+                generateItemCode();
+            })
+            .catch((error) => {
+                console.log("Load All Items Error : " + error);
+            });
+    };
+
+    const generateItemCode = () => {
+        axios.get(baseUrl + 'item/generateItemCode')
+            .then((resp) => {
+                let generatedId = resp.data.value;
+                if (generatedId === null) {
+                    setItemData({ ...itemData, code: "I00-001" });
+                } else {
+                    let tempId = parseInt(generatedId.split("-")[1]) + 1;
+                    let newId = tempId <= 9 ? "I00-00" + tempId : tempId <= 99 ? "I00-0" + tempId : "I00-" + tempId;
+                    setItemData({ ...itemData, code: newId });
+                }
+                setItemData(prevState => ({
+                    ...prevState,
+                    description: '',
+                    qty: '',
+                    unitPrice: ''
+                }));
+            })
+            .catch((error) => {
+                console.log("Generate Item Code Error : ", error);
+            });
+    };
+
+    const tableListener = (code, description, qty, unitPrice) => {
+        setItemData({ code, description, qty, unitPrice });
+    };
+
+    const clearInputFields = () => {
+        generateItemCode();
+        setItemData(prevState => ({
+            ...prevState,
+            description: '',
+            qty: '',
+            unitPrice: ''
+        }));
+        setSearchItemByCode('');
+    }
+
+    return {
+        handleChange,
+        itemValidations,
+        itemData,
+        saveItem,
+        updateItem,
+        deleteItem,
+
+        searchItemByCode,
+        setSearchItemByCode,
+        searchItem,
+        allItems,
+        loadAllItems,
+        tableListener,
+        clearInputFields,
+    };
+}
--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,65 +1,67 @@
-import { Container, Form, Button, Table, Row, Col } from 'react-bootstrap';
+import { Container, Form, Table, Row, Col, Button } from 'react-bootstrap';
+import { ItemController } from '../controllers/ItemController';
 
 export default function ItemPage() {
+    const { handleChange, itemValidations, itemData, saveItem, updateItem, deleteItem, searchItemByCode, setSearchItemByCode, searchItem,
+        allItems, loadAllItems, tableListener, clearInputFields } = ItemController();
     return (
-        <main className="container-fluid" id="ItemSection">
+        <main className="container-fluid">
             <Container className="d-flex flex-grow-1 flex-column">
                 <div className="position-relative d-inline-block mt-3 mt-lg-4 mt-md-4">
                     <h2 id="H1-1"><i className="bi bi-boxes"></i> Item Management</h2>
                 </div>
             </Container>
-            <Container className="d-flex flex-grow-1 flex-column sectionBorder mb-5 mt-2" style={{ backgroundColor: '#f0f0f0' }}>
+            <Container className="d-flex flex-grow-1 flex-column mb-5 mt-2 sectionBorder" style={{ backgroundColor: '#f0f0f0' }}>
                 <Row className="justify-content-around" style={{ height: 'max-content' }}>
                     {/* Item Search */}
                     <Col xs={10} md={6} lg={5} className="p-0 mt-5 sectionBorder" style={{ backgroundColor: 'white', height: 'max-content' }}>
-                        <Row className="mb-3 ms-3 me-3 mt-3">
+                        <Row className="m-3">
                             <Form.Label className="fw-bold">Search Item</Form.Label>
                             <div className="input-group">
-                                <Form.Control className="me-3 mt-2 rounded-2 w-50" id="txtSearchItemCode" type="search" placeholder="Input Item Code" />
-                                <Button variant="secondary" className="mt-2 rounded-2 w-25" id="btnSearchItem" type="button">Search</Button>
+                                <Form.Control className="me-3 mt-2 rounded-2 w-50" type="search" placeholder="Input Item Code" onKeyDown={(e) =>
+                                    e.key === 'Enter' && searchItem()} value={searchItemByCode} onChange={(e) => setSearchItemByCode(e.target.value)} />
+                                <Button variant="secondary" className="mt-2 rounded-2 w-25" type="button" onClick={searchItem}>Search</Button>
                             </div>
-                            <Button variant="success" className="mt-3 ms-3 rounded-2 w-25" id="btnGetAllItems" type="button">Get All</Button>
+                            <Button variant="success" className="mt-3 ms-3 rounded-2 w-25" type="button" onClick={loadAllItems}>Get All</Button>
                         </Row>
                     </Col>
-
                     {/* Item Form */}
                     <Col xs={10} md={5} lg={4} className="p-0 mt-5 mb-4 sectionBorder" style={{ backgroundColor: 'white' }}>
-                        <Form id="itemForm">
-                            <Form.Group className="mb-3 ms-3 me-3 mt-3">
-                                <Form.Label className="fw-bold" htmlFor="txtItemCode">Item Code</Form.Label>
-                                <Form.Control className="form-control" id="txtItemCode" name="code" type="text" />
-                                <span className="control-error"></span>
+                        <Form>
+                            <Form.Group className="m-3" controlId="code">
+                                <Form.Label className="fw-bold">Item Code</Form.Label>
+                                <Form.Control className="form-control" type="text" disabled={true} value={itemData.code} onChange={handleChange} />
+                                <span className="control-error">{itemValidations.code}</span>
                             </Form.Group>
-                            <Form.Group className="mb-3 ms-3 me-3 mt-3">
-                                <Form.Label className="fw-bold" htmlFor="txtItemDescription">Item Description</Form.Label>
-                                <Form.Control className="form-control" id="txtItemDescription" name="description" type="text" />
-                                <span className="control-error"></span>
+                            <Form.Group className="m-3" controlId="description">
+                                <Form.Label className="fw-bold">Item Description</Form.Label>
+                                <Form.Control className="form-control" type="text" value={itemData.description} onChange={handleChange} />
+                                <span className="control-error">{itemValidations.description}</span>
                             </Form.Group>
-                            <Form.Group className="mb-3 ms-3 me-3 mt-3">
-                                <Form.Label className="fw-bold" htmlFor="txtItemQuantity">Item Quantity</Form.Label>
-                                <Form.Control className="form-control" id="txtItemQuantity" name="qty" type="text" />
-                                <span className="control-error"></span>
+                            <Form.Group className="m-3" controlId="qty">
+                                <Form.Label className="fw-bold">Item Quantity</Form.Label>
+                                <Form.Control className="form-control" type="text" value={itemData.qty} onChange={handleChange} />
+                                <span className="control-error">{itemValidations.qty}</span>
                             </Form.Group>
-                            <Form.Group className="mb-3 ms-3 me-3 mt-3">
-                                <Form.Label className="fw-bold" htmlFor="txtItemUnitPrice">Unit Price</Form.Label>
-                                <Form.Control className="form-control" id="txtItemUnitPrice" name="unitPrice" type="text" />
-                                <span className="control-error"></span>
+                            <Form.Group className="m-3" controlId="unitPrice">
+                                <Form.Label className="fw-bold">Unit Price</Form.Label>
+                                <Form.Control className="form-control" type="text" value={itemData.unitPrice} onChange={handleChange} />
+                                <span className="control-error">{itemValidations.unitPrice}</span>
                             </Form.Group>
                             <div className="m-4 text-center">
-                                <Button variant="primary" className="mt-2 m-2 w-25" id="btnAddItem" type="button">Add</Button>
-                                <Button variant="warning" className="mt-2 m-2 w-25" id="btnUpdateItem" type="button">Update</Button>
-                                <Button variant="danger" className="mt-2 m-2 w-25" id="btnDeleteItem" type="button">Delete</Button>
-                                <Button variant="secondary" className="mt-2 m-2 w-25" id="btnClearAllItem" type="button">Clear All</Button>
+                                <Button variant="primary" className="mt-2 m-2 w-25" type="button" onClick={saveItem}>Add</Button>
+                                <Button variant="warning" className="mt-2 m-2 w-25" type="button" onClick={updateItem}>Update</Button>
+                                <Button variant="danger" className="mt-2 m-2 w-25" type="button" onClick={deleteItem}>Delete</Button>
+                                <Button variant="secondary" className="mt-2 m-2 w-25" type="button" onClick={clearInputFields}>Clear All</Button>
                             </div>
                         </Form>
                     </Col>
                 </Row>
-
                 <Row className="mt-5 mb-5 mt-md-2 mt-lg-4 justify-content-around">
                     {/* Item Table */}
                     <Col xs={11} className="justify-content-around sectionBorder" style={{ background: 'white' }}>
                         <div className="ItemTableDiv">
-                            <Table bordered hover className="mt-3">
+                            <Table bordered hover className="mt-3 table-cursor">
                                 <thead className="text-light table-dark text-center table-bordered">
                                     <tr>
                                         <th className="w-25">Item Code</th>
@@ -68,7 +70,16 @@ export default function ItemPage() {
                                         <th className="w-25">Unit Price</th>
                                     </tr>
                                 </thead>
-                                <tbody id="itemTable"></tbody>
+                                <tbody>
+                                    {allItems.map(item => (
+                                        <tr key={item.code} onClick={() => tableListener(item.code, item.description, item.qty, item.unitPrice)}>
+                                            <td>{item.code}</td>
+                                            <td>{item.description}</td>
+                                            <td>{item.qty}</td>
+                                            <td>{item.unitPrice}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
                             </Table>
                         </div>
                     </Col>
